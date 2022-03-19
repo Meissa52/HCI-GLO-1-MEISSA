be3d56ca4db0dbe5e0a80ad23830be1e6da1e5be
Meissa52 <47987637+Meissa52@users.noreply.github.com>
2022-03-19 12:09:29 -0400

    Initial commit

---
+++ html/js/init.js
@@ -0,0 +1,8 @@
+(function($){
+  $(function(){  
+  
+    $('.parallax').parallax();	
+	$('select').material_select();
+	$('input#input_text, textarea#textarea1').characterCounter();
+  });
+})(jQuery);
